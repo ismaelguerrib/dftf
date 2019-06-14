b43c3c06ef0bd53900ea93fe6d2e6ea42b348721
Ismaël Guerrib <ismael.guerrib@hotmail.fr>
2019-06-14 16:21:03 +0200

    yo

--- main.js
+++ main.js
@@ -139,7 +139,7 @@ function create() {
 function update() {
   if (lives === 0) {
     setTimeout(() => {});
-    window.location = "/";
+    window.location = "https://ismaelguerrib.github.io/dftf/";
   }
   setLive();
   fish.setVelocityX(50, 50);
